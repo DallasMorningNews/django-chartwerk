728c157177ab8963720c1ebe319ca61eb2f9794e
Andrew Chavez <adchavez@gmail.com>
2017-06-02 11:00:02 -0500

    Fix task for Github template saving

--- chartwerk/tasks/github.py
+++ chartwerk/tasks/github.py
@@ -2,80 +2,107 @@
 from __future__ import absolute_import
 import json
 import logging
-import os
 
 from celery import shared_task
-from chartwerk.models import Template
 from django.conf import settings
-from github import Github
+from github import Github, GithubException, UnknownObjectException
+
+from chartwerk.models import Template
+
 
 logger = logging.getLogger(__name__)
 
-repository = False
-if 'CHARTWERK_GITHUB_USER' in os.environ and \
-        'CHARTWERK_GITHUB_PASSWORD' in os.environ:
+
+def get_github_credentials():
+    """Get the credentials kwargs we're going to pass to Github()"""
     try:
-        repo_name = settings.CHARTWERK_GITHUB_REPO
-        g = Github(
-            os.getenv('CHARTWERK_GITHUB_USER'),
-            os.getenv('CHARTWERK_GITHUB_PASSWORD')
-        )
-        user = g.get_user()
-        if settings.CHARTWERK_GITHUB_ORG:
-            user = g.get_organization(settings.CHARTWERK_GITHUB_ORG)
+        return dict(login_or_token=settings.CHARTWERK_GITHUB_TOKEN)
+    except AttributeError:
         try:
-            repository = user.get_repo(repo_name)
-        except:
-            repository = user.create_repo(repo_name)
+            return dict(
+                login_or_token=settings.CHARTWERK_GITHUB_USER,
+                password=settings.CHARTWERK_GITHUB_PASSWORD
+            )
+        except AttributeError:
+            pass
+
+
+def get_repository():
+    """Get the Github repo specified in settings, adding if it doesn't exist"""
+    try:
+        repo_name = getattr(
+            settings,
+            'CHARTWERK_GITHUB_REPO',
+            'chartwerk_chart-templates'
+        )
+    except AttributeError:
+        return None
+
+    try:
+        g = Github(**get_github_credentials())
+
+        if hasattr(settings, 'CHARTWERK_GITHUB_ORG'):
+            user = g.get_organization(settings.CHARTWERK_GITHUB_ORG)
+        else:
+            user = g.get_user()
+
+        try:
+            return user.get_repo(repo_name)
+        except UnknownObjectException:
             logging.info("Creating repository {}".format(repo_name))
-    except:
+            return user.create_repo(repo_name)
+    except GithubException:
         logging.exception("Unable to configure Github connection.")
 
 
 def commit_script(path, script):
     """Commit script string to github repo."""
+    repository = get_repository()
+
     try:
-        try:
-            sha = repository.get_contents(path).sha
-            repository.update_file(
-                path=path,
-                message='Template update',
-                content=script,
-                sha=sha
-            )
-        except:
-            repository.create_file(
-                path=path,
-                message='Template initial commit',
-                content=script
-            )
-            logging.info("Initial commit of new file {}".format(path))
-    except Exception:
-        logging.exception("Unable to commit to github repo")
+        sha = repository.get_contents(path).sha
+        repository.update_file(
+            path=path,
+            message='Template update',
+            content=script,
+            sha=sha
+        )
+    except GithubException:
+        repository.create_file(
+            path=path,
+            message='Template initial commit',
+            content=script
+        )
+        logging.info("Initial commit of new file {}".format(path))
 
 
 @shared_task
 def commit_template(pk):
-    template = Template.objects.get(pk=pk)
     """Commit template scripts to github."""
-    if repository:
-        commit_script(
-            '/{}/draw.js'.format(template.slug),
-            template.data['scripts']['draw']
-        )
-        commit_script(
-            '/{}/helper.js'.format(template.slug),
-            template.data['scripts']['helper']
-        )
-        commit_script(
-            '/{}/chart.html'.format(template.slug),
-            template.data['scripts']['html']
-        )
-        commit_script(
-            '/{}/styles.css'.format(template.slug),
-            template.data['scripts']['styles']
-        )
-        commit_script(
-            '/{}/template.json'.format(template.slug),
-            json.dumps(template.data)
-        )
+    repository = get_repository()
+
+    if repository is None:
+        return
+
+    template = Template.objects.get(pk=pk)
+
+    commit_script(
+        '/{}/draw.js'.format(template.slug),
+        template.data['scripts']['draw']
+    )
+    commit_script(
+        '/{}/helper.js'.format(template.slug),
+        template.data['scripts']['helper']
+    )
+    commit_script(
+        '/{}/chart.html'.format(template.slug),
+        template.data['scripts']['html']
+    )
+    commit_script(
+        '/{}/styles.css'.format(template.slug),
+        template.data['scripts']['styles']
+    )
+    commit_script(
+        '/{}/template.json'.format(template.slug),
+        json.dumps(template.data)
+    )
