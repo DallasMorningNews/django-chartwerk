8c674cf4ded44b509d3b8e080fd078c0880308e8
Jon McClure <jmcclure@politico.com>
2017-07-11 16:32:47 -0400

    Closes #42. Closes #41. Updates chartwerk-editor to 0.1.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 REPO_URL = 'https://github.com/DallasMorningNews/django-chartwerk/'
 
-PYPI_VERSION = '0.4.3'
+PYPI_VERSION = '0.4.4'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -38,7 +38,7 @@ setup(
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: AGPLv3',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
