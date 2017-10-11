b2b626519b29f6a78da52626469638d7d1814adf
Andrew Chavez <adchavez@gmail.com>
2017-10-11 14:53:08 -0500

    Drop deprecated user_by_email tag

--- chartwerk/templatetags/chartwerk_tags.py
+++ chartwerk/templatetags/chartwerk_tags.py
@@ -10,18 +10,3 @@ register = template.Library()
 @register.filter
 def jsonify(value):
     return json.dumps(value)
-
-
-@register.filter
-def user_by_email(email):
-    if email is None:
-        return 'DEBUGGER'
-    user = User.objects.filter(email=email).first()
-    if user:
-        if user.first_name != '' and user.last_name != '':
-            return '{} {}'.format(
-                user.first_name,
-                user.last_name
-            )
-    else:
-        return email
