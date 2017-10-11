2e34d9f4329dfe7979391653769ab6cc0d8b9e95
Andrew Chavez <adchavez@gmail.com>
2017-10-11 14:29:53 -0500

    Drop deprecated template tag

--- chartwerk/templatetags/chartwerk_tags.py
+++ chartwerk/templatetags/chartwerk_tags.py
@@ -1,13 +1,8 @@
 import json
-import os
 
-from chartwerk.models import Template
 from django import template
-from django.conf import settings
 from django.contrib.auth.models import User
-from django.contrib.staticfiles.templatetags.staticfiles import static
 
-DOMAIN = settings.CHARTWERK_DOMAIN
 
 register = template.Library()
 
@@ -30,18 +25,3 @@ def user_by_email(email):
             )
     else:
         return email
-
-
-@register.filter
-def get_icon(title):
-    default = os.path.join(
-        DOMAIN,
-        static('chartwerk/img/chartwerk_100.png')[1:]
-    )
-    if title is None:
-        return default
-    template = Template.objects.filter(title=title).first()
-    if template and template.icon:
-        return Template.objects.filter(title=title).first().icon.url
-    else:
-        return default
