6a601eeecc74c7887b28bb20922d3bd9b6a42f3f
Jon McClure <jmcclure@politico.com>
2017-12-28 18:44:14 -0500

    0.5.18

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 REPO_URL = 'https://github.com/DallasMorningNews/django-chartwerk/'
 
-PYPI_VERSION = '0.5.17'
+PYPI_VERSION = '0.5.18'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
